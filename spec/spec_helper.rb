36683269031d7476828b83c32860870d4c8dc99c
Russell Garner <rgarner@zephyros-systems.co.uk>
2016-04-07 19:03:30 +0100

    Explicit .should config/rspec-its

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,13 @@
 require 'webmachine'
 require 'webmachine/actionview'
 require 'dummy/app'
+require 'rspec/its'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
+  config.mock_with :rspec do |c|
+    c.syntax = :should
+  end
+end
